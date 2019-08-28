f1cfb66e530c193ce872ab8dd8666d666c4a677b
Ryan Ashcraft <ryan@ryanashcraft.me>
2019-08-28 08:43:40 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "Amplitude",
     products: [.library(name: "Amplitude", targets: ["Amplitude"])],
     targets: [
-        .target(name: "Amplitude", path: "Amplitude"))
+        .target(name: "Amplitude", path: "Amplitude")
     ],
     swiftLanguageVersions: [4, 5]
 )
